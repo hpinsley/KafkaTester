5d0100dad636eb4bca4a65dd231eef347238fa71
Howard Pinsley <hpinsley@gmail.com>
2019-04-03 21:11:46 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
